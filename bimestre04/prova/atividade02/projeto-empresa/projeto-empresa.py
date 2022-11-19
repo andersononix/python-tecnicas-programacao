7778a5182977601f91139ab5ca91ce34783dff47
andersononix <andersonakauan13@gmail.com>
2022-11-19 10:52:48 -0300

    .
    .

--- bimestre04/prova/atividade02/projeto-empresa/projeto-empresa.py
+++ bimestre04/prova/atividade02/projeto-empresa/projeto-empresa.py
@@ -1,35 +1,3 @@
-#Funções
-def menuPrincipal():
-    print("MENU PRINCIPAL")
-    print("1 - Cadastrar")
-    print("2 - Excluir")
-    print("3 - Altera")
-    print("4 - Imprimir")
-    print("5 - Sair")
-
-def menuLocal():
-    print("Locais para viajar")
-    print("1 - EUA              (R$ 5.000,00)")
-    print("2 - África do Sul    (R$ 10.000,00)")
-    print("3 - Japão            (R$ 15.000,00)")
-
-def menuClasse():
-    print("Nosso servisos de voos")
-    print("1 - Classe econômica     (Valor da pasagem)")
-    print("2 - Classe executiva     (Aumento na pasagem de 30%)")
-    print("3 - Primeira classe      (Aumento na pasagem de 50%)")
-
-def menuHospedagem():
-    print("Tipos de Hospedagem")
-    print("1 - 3 estrelas   (R$ 100,00 por dia)")
-    print("2 -  4 estrelas  (R$ 200,00 por dia)")
-    print("3 -  5 estrelas  (R$ 300,00)")
-
-def escolhaLocal(opcao):
-    if(opcao == 1):
-        print()
-
-
 #Listas
 nomes = []
 datasPastidas = []
@@ -37,6 +5,53 @@ locais = []
 classes = []
 hospedagens = []
 qtdsDias = []
+valorFinal = []
+
+# Funções
+def menuPrincipal():
+    print("\nMENU PRINCIPAL")
+    print("[1] - Cadastrar")
+    print("[2] - Excluir")
+    print("[3] - Altera")
+    print("[4] - Imprimir")
+    print("[5] - Sair")
+
+def menuLocal():
+    print("\nLocais para viajar")
+    print("[1] - EUA              (R$ 5.000,00)")
+    print("[2] - África do Sul    (R$ 10.000,00)")
+    print("[3] - Japão            (R$ 15.000,00)")
+
+def menuClasse():
+    print("\nNosso servisos de voos")
+    print("[1] - Classe econômica     (Valor da pasagem)")
+    print("[2] - Classe executiva     (Aumento na pasagem de 30%)")
+    print("[3] - Primeira classe      (Aumento na pasagem de 50%)")
+
+def menuHospedagem():
+    print("\nTipos de Hospedagem")
+    print("[1] - 3 estrelas   (R$ 100,00 por dia)")
+    print("[2] - 4 estrelas  (R$ 200,00 por dia)")
+    print("[3] - 5 estrelas  (R$ 300,00)")
+
+def Imprimir(nomes, datasPastidas, locais, hospedagens, qtdsDias):
+    for v in nomes:
+        print(f"Nome do cliente: {v}")
+    for v in datasPastidas:
+        print(f"Data de partida: {v}")
+    for v in locais:
+        print(f"Local: {v}")
+    for v in datasPastidas:
+        print(f"Classe: {v}")
+    for v in hospedagens:
+        print(f"Hospedagem: {v}")
+    for v in qtdsDias:
+        print(f"Quantidade de dias: {v}")
+        print(valor)
+
+def nemuNomes(nomes):
+    for c, v in enumerate(lista):
+        print(f"Na posição: {c} tem o nomes: {v}", end=" ")
 
 print("NOME DA EMPRESA")
 
@@ -46,25 +61,77 @@ while (resp == 1):
     usuario = str(input("\nUsúario: "))
     senha = str(input("Senha: "))
     if(usuario == "1") and (senha == "1"):
+
         #INICIO
         menuPrincipal()
         opcao = int(input("Digiti sua opção: "))
 
         #CASDASTRAR USUARIO
         if  (opcao == 1):
-            nomes.append(str(input("Nome do cliente: ")))
+            nomes.append(str(input("\nNome do cliente: ")))
             datasPastidas.append(str(input("Data de partida: ")))
+
             # Tipos de local
-            menuLocal()
-            opcao = int(input("Escolha sua opção: "))
-            escolhaLocal(opcao)
+            resp = 1
+            while (resp == 1):
+                menuLocal()
+                opcao = int(input("Escolha sua opção: "))
+                locais.append(opcao)
+                if (opcao == 1):
+                    valorLocal = 5000
+                    break
+                elif (opcao == 2):
+                    valorLocal = 10000
+                    break
+                elif (opcao == 3):
+                    valorLocal = 150000
+                    break
+                else:
+                    print("\nNão tem essa opção")
+                    resp = 1
+
             # Classes de voos
-            menuClasse()
-            classes.append(int(input("Escolha sua opção: ")))
+            resp = 1
+            while (resp == 1):
+                menuClasse()
+                opcao = int(input("Escolha sua opção: "))
+                classes.append(opcao)
+                if(opcao == 1):
+                    desconto = valorLocal
+                    break
+                elif(opcao == 2):
+                    desconto = valorLocal + (valorLocal * 0.3)
+                    break
+                elif(opcao == 3):
+                    desconto = valorLocal + (valorLocal * 0.5)
+                    break
+                else:
+                    print("\nNão tem essa opção")
+                    resp = 1
+
             # Hospedagem
-            menuHospedagem()
-            hospedagens.append(int(input("escolha sua opção: ")))
-            qtdsDias.append(str(input("Quantidade de dias: ")))
+            resp = 1
+            while (resp == 1):
+                menuHospedagem()
+                opcao = int(input("Escolha sua opção: "))
+                hospedagens.append(opcao)
+                if(opcao == 1):
+                    valorHospedagem = 100
+                    break
+                elif(opcao == 2):
+                    valorHospedagem = 200
+                    break
+                elif(opcao == 3):
+                    valorLocal = 300
+                    break
+                else:
+                    print("\nNão tem essa opção")
+                    resp = 1
+
+            qtdDias = int(input("Quantidade de dias: "))
+            qtdsDias.append(qtdDias)
+            valor = desconto + (valorHospedagem * qtdDias)
+            valorFinal.append(valor)
 
         #EXCLUIR USUARIO
         elif(opcao == 2):
@@ -72,23 +139,44 @@ while (resp == 1):
 
         #ALTERAR CADASTRO DO USUARIO
         elif (opcao == 3):
-            print()
+            resp = 1
+            while (resp == 1):
+                alterar = int(input("Escolha uma opção: "))
+                if(alterar == 1):
+                    nomes.pop(0)
+                    nome = str(input("Nome do cliente: "))
+                    nomes.append(0, nome)
+                    break
+                elif(alterar == 2):
+                    print()
+                    break
+                elif (alterar == 3):
+                    print()
+                    break
+                elif (alterar == 4):
+                    print()
+                    break
+                elif (alterar == 5):
+                    print()
+                    break
+                elif (alterar == 6):
+                    print()
+                    break
+                else:
+                    print("Nao tem essa opção")
+                    resp = 1
 
         #IMPRIMIR DADOS DO USUARIO
         elif (opcao == 4):
-            print()
+            Imprimir(nomes, datasPastidas, locais, hospedagens, qtdsDias)
 
         #SAIR
         else:
-            print()
+            print("Obigado")
+            break
     else:
         print("\nErro login incorreto")
         print("Tente novamente")
         resp = 1
-print(nomes)
-print(datasPastidas)
-print(locais)
-print(classes)
-print(hospedagens)
-print(qtdsDias)
+
 
--- bimestre04/prova/atividade02/projeto-empresa/rascunho.py
+++ bimestre04/prova/atividade02/projeto-empresa/rascunho.py
@@ -1,33 +1,35 @@
-lista_Economica = []
-#lista_Executiva = []
-#lista_Primeira = []
-login = str(input("login:"))
-senha = str(input("senha:"))
-op = 1
-if(login == 'admin')and(senha == 'admin'):
-    print("Menu\n1 - Cadastrar: Armazenar e Imprimir\n2 - Excluir")
-    while (op == 1):
-        data = str(input("Qual é a data de partida:"))
-        print("[1]-EUA\n[2]-África\ndo Sul\n[3]-Japão")
-        local = int(input("Qual o local que deseja:"))
-        print("[1]-Classe Econômica\n[2]-Executiva\ndo Sul\n[3]-Primeira classe")
-        classes = int(input("Escolha uma classe:"))
-        print("[1]-3 estrelas (R$ 100,00 por dia)\n[2]-4 estrelas(R$ 200,00 por dia)\n[3]-5 estrelas (R$ 300,00 por dia)")
-        hospedagem = int(input("Escolha um tipo:"))
-        if(hospedagem == 1):
-            hospedagem = 100
-        elif(hospedagem == 2):
-            hospedagem = 200
-        else:
-            hospedagem = 300
-        quantidade_dias = int(input("Quantidade de dias:"))
-        op = int(input("escolha uma op:"))
-        while (op == 2):
-            excluir = int(input("Oq vc deseja escluir:"))
-            if(excluir == 1):
-                lista_Economica.insert(excluir, "livre")
+'''
+lista = []
+lista1 = []
+dados = []
 
-print(f'{data}')
-print(f'{local}')
-print(f'{quantidade_dias}')
-print(lista_Economica)
+resp = 1
+
+while (resp == 1):
+    op = int(input("op: "))
+    if(op == 1):
+        lista.append(str(input("Qual o seu nome: ")))
+        dados.append(lista)
+        dados.append(lista1)
+    else:
+        print("Obrigado")
+        break
+    resp = 1
+for c, v in enumerate(lista):
+    print(f"Naposiçao {c} tem o nomes: {v}")
+'''
+nomes = []
+resp = 1
+while (resp == 1):
+    nomes.append(str(input("Nome do cliente: ")))
+    nomes.append(str(input("Nome do cliente: ")))
+    alterar = int(input("Qual aopção: "))
+    if(alterar == 1):
+        nome1 = str(input("Nome do cliente: "))
+        nomes.pop(0)
+        nomes.insert(0, nome1)
+        print(nomes)
+        break
+    else:
+        print("Nãõ tem na lista")
+        resp = 1
